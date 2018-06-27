404d4873316ec6cbc97903e2402160cf43e05490
20171105112 <903056683@qq.com>
2018-06-27 11:00:49 +0800

    计算评分代码
    输入数据  通过运行来计算评分均值

--- 2018.6.27/main.cpp
+++ 2018.6.27/main.cpp
@@ -6,10 +6,31 @@
 //  Copyright © 2018年 黄宏. All rights reserved.
 //
 
-#include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
+#include <stdio.h>
+    int main()
+    {
+        float score[10];
+        int i = 0;
+        float max = 0.0, min = 100.0, sum = 0.0;
+        printf("输入10个评分:");
+        for (; i < 10; ++i)
+        {
+            scanf("%f", &score[i]); //存入数组
+            if (max < score[i])
+            {
+                max = score[i]; //最大
+            }
+            if (min > score[i])
+            {
+                min = score[i]; //最小
+            }
+        }
+        for (i = 0; i < 10; ++i)
+        {
+            if (score[i] == max || score[i] == min)
+                continue;
+            sum += score[i];
+        }
+        printf("最后分数为:%f\n", sum/8.0);
+    }
